c758c74b9b4f08afb3593f07477c222c4697f642
Rivka Altshuler <brandrivka@gmail.com>
2022-10-03 20:32:41 +0300

    folder management continue

---
+++ RIVarX/Expression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RIVarX
+{
+    public class Expression<T> : IObservable<Signal<T>>
+    {
+        IObservable<Signal<T>> _stream;
+        public Expression(IObservable<Signal<T>> stream)
+        {
+            _stream = stream;
+        }
+
+        public IDisposable Subscribe(IObserver<Signal<T>> observer)
+        {
+            return _stream.Subscribe(observer);
+        }
+     
+    }
+
+
+}
---
+++ RIVarX/RIvar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Linq;
+
+
+
+namespace RIVarX
+{
+    public class RIVar<T> : ISubject<Signal<T>>
+    {
+        ISubject<Signal<T>> _subject;
+
+        /// <summary>
+        /// relate source to target, so that for any (new) change in the source, the target is updated
+        /// </summary>
+        public void Set(Expression<T> source)
+        {
+            var targetWithInitialValue = _subject.StartWith(new Signal<T>(default(T),new int[] { 0 }));
+
+            var streamOfChanges = Observable.CombineLatest(source, targetWithInitialValue, (valueInSource, valueInTarget) => new Tuple<Signal<T>, Signal<T>>(valueInSource, valueInTarget));
+
+            var streamOfChangesInSource = streamOfChanges.Where(change => change.Item1?.CompareTo(change.Item2) > 0).Select(change => change.Item1);
+
+            //streamOfChangesInSource.Subscribe(o=> {
+            //    System.IO.File.AppendAllText($"sets.txt", $"\r\ntrigger {o.Value} <{string.Join(",", o.PrioritySet)}>");
+            //    _subject.OnNext(o);
+            //    System.IO.File.AppendAllText($"sets.txt", $"\r\nafter trigger {o.Value} <{string.Join(",", o.PrioritySet)}>");
+            //});// update the target for changes in the source
+
+            streamOfChangesInSource.Subscribe(_subject.OnNext);// update the target for changes in the source
+        }
+
+    
+
+        public RIVar()
+        {
+            _subject = new Subject<Signal<T>>();
+        }
+        public void OnCompleted()
+        {
+            _subject.OnCompleted();
+        }
+
+        public void OnError(Exception error)
+        {
+            _subject.OnError(error);
+        }
+
+        public void OnNext(Signal<T> value)
+        {
+            _subject.OnNext(value);
+        }
+
+        public IDisposable Subscribe(IObserver<Signal<T>> observer)
+        {
+            return _subject.Subscribe(observer);
+        }
+    }
+}
+
